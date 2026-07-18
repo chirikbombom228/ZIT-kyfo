bcba5beaf19b9e316734472526ca9e386370daa5
chirikbombom228 <olesa.vorobeva.2006@gmail.com>
2026-07-18 14:33:41 +0400

    Добавила аннотации в ServicePointController

--- src/main/java/zit/kyfo/backend/controller/ServicePointsController.java
+++ src/main/java/zit/kyfo/backend/controller/ServicePointsController.java
@@ -1,5 +1,8 @@
 package zit.kyfo.backend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,11 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/points")
 public class ServicePointsController {
 
+    @Operation(summary = "Проверить баланс по номеру билета",
+            description = "Возвращает баланс определенного билета по его номеру")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Талон успешно найден"),
+            @ApiResponse(responseCode = "404", description = "Талон не найден")
+    })
     @GetMapping("/checkBalance")
     public Object checkBalance(@RequestParam("ticketNumber") String ticketNumber) {
         return null;
     }
+    //
 
+    @Operation(summary = "Списать сумму с посадочного талона",
+            description = "Выполняет списание денежных средств с баланса, привязанного к конкретному посадочному талону")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Талон успешно найден. Списание выполнено успешно. Сумма списана с баланса"),
+            @ApiResponse(responseCode = "404", description = "Талон не найден"),
+            @ApiResponse(responseCode = "409", description = "Недостаточно средств на балансе для списания (или талон уже аннулирован).")
+    })
     @PostMapping("/pay")
     public Object pay(@RequestBody Object request) {
         return null;
